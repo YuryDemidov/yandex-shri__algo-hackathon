64ff34b808e4ba322ce3bc4ba3e12c0c237dc111
Yury Demidov <demidov_uriy@poisondrop.ru>
2021-07-04 16:10:44 +0300

    Brackets consistensy

--- src/js/3.js
+++ src/js/3.js
@@ -1,1 +1,35 @@
-console.log('3.js');
+const BRACKETS = ['{', '}', '[', ']', '(', ')'];
+const BRACKETS_MATCH = {
+  '}': '{',
+  ']': '[',
+  ')': '('
+};
+
+function isBracketsConsistent(string) {
+  const bracketsStack = [];
+  let hasError = false;
+
+  for (let char of string) {
+    if (BRACKETS.indexOf(char) === -1) {
+      continue;
+    }
+
+    if (!BRACKETS_MATCH[char]) {
+      bracketsStack.push(char);
+    } else if (BRACKETS_MATCH[char] === bracketsStack[bracketsStack.length - 1]) {
+      bracketsStack.pop();
+    } else {
+      hasError = true;
+      break;
+    }
+  }
+
+  return bracketsStack.length === 0 && !hasError;
+}
+
+console.log(isBracketsConsistent('{([])}{}'));
+console.log(isBracketsConsistent('(({{([])}{()}}))'));
+console.log(isBracketsConsistent('{([{])}{}'));
+console.log(isBracketsConsistent('{([])}{})'));
+console.log(isBracketsConsistent(''));
+console.log(isBracketsConsistent('}'));
