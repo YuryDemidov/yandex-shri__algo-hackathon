702b68cec094d3c61f43142025cbb74085685f98
Yury Demidov <demidov_uriy@poisondrop.ru>
2021-07-04 18:37:26 +0300

    Reverse linked list

--- src/js/7.js
+++ src/js/7.js
@@ -1,1 +1,35 @@
-console.log('7.js');
+function reverseSinglyLinkedList(list) {
+  let previous = null;
+  let current = list;
+  let next = list.next;
+
+  while (next) {
+    const temp = next.next;
+    current.next = previous;
+    next.next = current;
+    previous = current;
+    current = next;
+    next = temp;
+  }
+
+  return current;
+}
+
+const LINKED_LIST = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: {
+          value: 5,
+          next: null
+        }
+      }
+    }
+  }
+};
+
+console.log(reverseSinglyLinkedList(LINKED_LIST));
