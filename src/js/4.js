b5d2a33e087a5259de3f8f75b2a29396bbdac12b
Yury Demidov <demidov_uriy@poisondrop.ru>
2021-07-04 16:32:17 +0300

    Deep first tree traverse

--- src/js/4.js
+++ src/js/4.js
@@ -1,1 +1,49 @@
-console.log('4.js');
+function deepFirstTraverse(tree, treeArray = []) {
+  treeArray.push(tree.value);
+
+  if (tree.children) {
+    for (let i = 0; i < tree.children.length; i++) {
+      deepFirstTraverse(tree.children[i], treeArray);
+    }
+  }
+
+  return treeArray;
+}
+
+const TREE = {
+  value: 4,
+  children: [
+    {
+      value: 2,
+      children: [
+        {
+          value: 1,
+          children: []
+        },
+        {
+          value: 3,
+          children: []
+        }
+      ]
+    },
+    {
+      value: 5,
+      children: [
+        {
+          value: 6,
+          children: []
+        },
+        {
+          value: 7,
+          children: []
+        },
+        {
+          value: 8,
+          children: []
+        }
+      ]
+    }
+  ]
+};
+
+console.log(deepFirstTraverse(TREE));
