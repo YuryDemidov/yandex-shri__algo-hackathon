552d324fb7867f1f02ef7d0737e825ee3b5a0d68
Yury Demidov <demidov_uriy@poisondrop.ru>
2021-07-04 16:56:59 +0300

    Prime numbers

--- src/js/5.js
+++ src/js/5.js
@@ -1,1 +1,29 @@
-console.log('5.js');
+function getPrimeNumbers(max) {
+  const primeNumbers = [1, 2];
+
+  if (max === 2) {
+    return primeNumbers;
+  }
+
+  let num = 3;
+
+  while (num < max) {
+    let isPrime = true;
+
+    for (let i = 1; i < primeNumbers.length; i++) {
+      if (num % primeNumbers[i] === 0) {
+        isPrime = false;
+      }
+    }
+
+    if (isPrime) {
+      primeNumbers.push(num);
+    }
+
+    num += 2;
+  }
+
+  return primeNumbers;
+}
+
+console.log(getPrimeNumbers(200));
