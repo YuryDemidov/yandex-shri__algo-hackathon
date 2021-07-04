89d24bd6966175809ead449f6038eb4f358faf2d
Yury Demidov <demidov_uriy@poisondrop.ru>
2021-07-04 17:49:35 +0300

    Inserts sorting

--- src/js/6.js
+++ src/js/6.js
@@ -1,1 +1,29 @@
-console.log('6.js');
+function insertsSorting(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+
+  const sorted = [arr[0]];
+
+  for (let i = 1; i < arr.length; i++) {
+    let isMax = true;
+
+    for (let j = 0; j < sorted.length; j++) {
+      if (arr[i] < sorted[j]) {
+        sorted.splice(j, 1, arr[i], sorted[j]);
+        isMax = false;
+        break;
+      }
+    }
+
+    isMax && sorted.push(arr[i]);
+  }
+
+  return sorted;
+}
+
+console.log(insertsSorting([7, 1, 4, 3, 13, 11, 1, 1, 1]));
+console.log(insertsSorting([]));
+console.log(insertsSorting([2]));
+console.log(insertsSorting([5, 4, 3, 2, 1]));
+console.log(insertsSorting([1, 1, 1, 1, 1, 1, 1, 1]));
