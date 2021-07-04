8e232f2791a3faf2f0a8d0ffe8858a9eef517263
Yury Demidov <demidov_uriy@poisondrop.ru>
2021-07-04 19:26:19 +0300

    Binary search

--- src/js/8.js
+++ src/js/8.js
@@ -1,20 +1,28 @@
 function binarySearch(arr, term) {
-  const mid = Math.floor((arr.length - 1) / 2);
+  let left = 0;
+  let right = arr.length - 1;
 
-  if (arr.length > 1) {
-    if (arr[mid] < term) {
-      return binarySearch(arr.slice(mid), term);
-    } else if (arr[mid] > term) {
-      return binarySearch(arr.slice(0, mid), term);
-    } else {
+  while (left <= right) {
+    let mid = Math.floor((right + left) / 2);
+
+    if (arr[mid] === term) {
       return mid;
+    } else if (arr[mid] > term) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
     }
   }
-  if (arr[0] === term) {
-    return mid;
-  }
-
   return -1;
 }
 
-console.log(binarySearch([1, 2, 15, 23, 42, 77, 80, 91, 112], 23));
+console.log(binarySearch([1, 2, 15, 23, 42, 77, 80, 91, 112], 80));
+console.log(binarySearch([1, 2, 15, 23, 42, 77, 80, 91, 112], 1));
+console.log(binarySearch([1, 2, 15, 23, 42, 77, 80, 91, 112], 112));
+console.log(binarySearch([1, 2, 15, 23, 42, 77, 80, 91, 112], 42));
+console.log(binarySearch([1, 2, 15, 23, 42, 77, 80, 91, 112], 10));
+console.log(binarySearch([1, 2, 15, 23, 42, 77, 80, 91], 80));
+console.log(binarySearch([1, 2, 15, 23, 42, 77, 80, 91], 91));
+console.log(binarySearch([1, 2, 15, 23, 42, 77, 80, 91], 1));
+console.log(binarySearch([1, 2, 15, 23, 42, 77, 80, 91], 42));
+console.log(binarySearch([1, 2, 15, 23, 42, 77, 80, 91], 10));
